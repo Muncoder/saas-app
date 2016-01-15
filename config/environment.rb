115be84bbf437ef90fed53cbd23e59c14acd8535
Munir <munirchittagong@yahoo.com>
2016-01-15 15:17:59 +0000

    Add email setup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+	:address => 'smtp.sendgrid.net',
+	:user_name => "app46191193@heroku.com",
+	:password => "cu2rahty6541",
+	:enable_starttls_auto => true,
+	:port => 587,
+	:authentication => 'plain'
+}
